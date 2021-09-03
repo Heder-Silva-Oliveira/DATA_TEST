6ba8eee6512638a79cd77af54b8fbf9617143c03
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-09-03 17:07:55 -0300

    003

--- Programa.py
+++ Programa.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import pandera as pa
 
 
-df = pd.read_csv("ocorrencia.csv", sep=';', parse_dates=['ocorrencia_dia'])
-print(df.ocorrencia_dia.dt.month)
+df = pd.read_csv("ocorrencia.csv", sep=';', parse_dates=['ocorrencia_dia'], dayfirst=True)
+df.pop('ocorrencia_saida_pista')
+df.pop('total_aeronaves_envolvidas')
 
+schema = pa.DataFrameSchema(
+    columns={
+        "codigo_ocorrencia":pa.Column(pa.Int)
+    }
+)
+print(schema.validate(df))
