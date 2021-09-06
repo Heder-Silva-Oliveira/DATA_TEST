32a3942434a5bab4707537f481fcdb748afb972f
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-09-06 18:26:40 -0300

    006

--- Programa.py
+++ Programa.py
@@ -9,15 +9,16 @@ df.pop('codigo_ocorrencia1')
 
 schema = pa.DataFrameSchema(
     columns={
-        "codigo_ocorrencia" : pa.Column(pa.Int),
-        "codigo_ocorrencia2" : pa.Column(pa.Int),
-        "ocorencia_classificacao" : pa.Column(pa.String),
-        "ocorencia_cidade" : pa.Column(pa.String),
+        "codigo_ocorrencia": pa.Column(pa.Int),
+        "codigo_ocorrencia2": pa.Column(pa.Int),
+        "ocorencia_classificacao": pa.Column(pa.String),
+        "ocorencia_cidade": pa.Column(pa.String),
         "ocorrencia_uf": pa.Column(pa.String, pa.Check.str_length(2,2), nullable=True),
         "ocorrencia_aerodromo": pa.Column(pa.String, nullable=True),
-        "ocorrencia_dia" : pa.Column(pa.DateTime),
+        "ocorrencia_dia": pa.Column(pa.DateTime),
         "ocorrencia_hora": pa.Column(pa.String, pa.Check.str_matches(r'^([0-1]?[0-9]|[2][0-3]):([0-5][0-9])(:[0-5][0-9])?$'), nullable=True),
-        "total_recomendacoes" : pa.Column(pa.Int)
+        "total_recomendacoes": pa.Column(pa.Int)
     }
 )
+
 print(schema.validate(df))
